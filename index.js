ff1df192de7d42fe70e4d9ff44caf6121ddc1f51
Tilde <tilde@thuryism.net>
2025-10-13 14:18:41 -0700

    getting traces working

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require("express");
 const path = require("path");
 const serveStatic = require("serve-static");
+const { init } = require("@launchdarkly/node-server-sdk");
+const {
+  Observability,
+  LDObserve,
+} = require("@launchdarkly/observability-node");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
 app.use(serveStatic(path.join(__dirname, "public")));
 
+const client = init(process.env.LAUNCHDARKLY_SDK_KEY, {
+  plugins: [
+    new Observability({
+      serviceName: "expressjs-launchdarkly-starter",
+    }),
+  ],
+});
+
 app.get("/", (req, res) => {
+  const { span } = LDObserve.startWithHeaders("homepage.render", req.headers);
+
   res.send("Hello World!");
+  span.end();
 });
 
 const port = 3000;
 const server = app.listen(port, function (err) {
   if (err) console.log("Error in server setup");
   console.log(`Server listening on http://localhost:${port}`);
+});
+
+// Graceful shutdown
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received, shutting down gracefully");
+  server.close(() => {
+    observability.flush();
+    observability.close();
+    console.log("Process terminated");
+    process.exit(0);
+  });
 });
